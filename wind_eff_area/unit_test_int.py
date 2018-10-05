c96e50ed2cf81c45ab57956c6965f69b1bcf283f
jprchlik <japrchlik@gmail.com>
2018-10-05 17:11:08 -0400

    Modified with the new coordiante system for integrator

--- wind_eff_area/unit_test_int.py
+++ wind_eff_area/unit_test_int.py
@@ -9,15 +9,15 @@ def test_midpoint():
      samp = 200
      args = []
      x_lo = 0
-     x_hi = 3.5
+     x_hi = 40.5
      y_lo = lambda x : x*0
      y_hi = lambda x : 2.0*np.pi+x*0
      z_lo =  lambda x,y : x*y*0
      z_hi =  lambda x,y : x*0*y+np.pi
 
-     def dA_Sphere(phi, theta):
+     def dA_Sphere(theta,phi):
        return  np.sin(phi)
-     def dV_Sphere(phi, theta, r):
+     def dV_Sphere( theta,phi,r):
        return r * r * dA_Sphere(phi, theta)
 
      val, err = tplquad(dV_Sphere, 0.0, 3.5, y_lo,
@@ -52,10 +52,11 @@ def test_midpoint():
 
     
      print(int_test,int_real,int_runu)
-     print(np.abs(int_runu-int_test)/np.abs(int_test))
+     print(np.abs(int_runu-int_real)/np.abs(int_real))
      print(error)
      #return int_runu,arr
-     assert np.abs(int_runu-int_test)/np.abs(int_test) < 1.e-4
+     assert np.abs(int_runu-int_real)/np.abs(int_real) < 1.e-4
+     assert np.abs(int_test-int_real)/np.abs(int_real) < 1.e-4
 
 
 if __name__ == '__main__':
