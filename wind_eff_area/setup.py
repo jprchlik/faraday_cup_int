10e89907c6e94ae3a221f23f5ae586a7f2a89f23
jprchlik <japrchlik@gmail.com>
2018-10-26 17:21:39 -0400

    Added geo_dist.pyx to setup.py

--- wind_eff_area/setup.py
+++ wind_eff_area/setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(name="mid_point_loop", ext_modules=cythonize('mid_point_loop.pyx'),)
+#setup(name="mid_point_loop", ext_modules=cythonize('mid_point_loop.pyx'),)
+setup(name="geo_dist", ext_modules=cythonize('geo_dist.pyx'),)
