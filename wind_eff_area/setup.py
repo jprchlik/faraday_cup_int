0e908a0ca77411fae509a537354b9eff59fc24b6
jprchlik <japrchlik@gmail.com>
2018-11-02 09:16:19 -0400

    Added setup for python cython modules

--- wind_eff_area/setup.py
+++ wind_eff_area/setup.py
@@ -1,5 +1,7 @@
+from distutils.core import setup, Extension
+import numpy as np
 from distutils.core import setup
 from Cython.Build import cythonize
 
-#setup(name="mid_point_loop", ext_modules=cythonize('mid_point_loop.pyx'),)
+setup(name="mid_point_loop", ext_modules=cythonize('mid_point_loop.pyx'),)
 setup(name="geo_dist", ext_modules=cythonize('geo_dist.pyx'),)
