f788c47e34f063a9b7965e4f4bcb16b8aa8ce7ce
jprchlik <japrchlik@gmail.com>
2019-03-22 11:56:54 -0400

    Updated config.py with something

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.imgmath',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.githubpages',
+    'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
